684e2ca6dbcdc849816b58559e09de1d8a52f325
pixleyllc <spam@potuszack.com>
2025-08-27 02:40:04 -0700

    Create app.js

---
+++ public/app.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDHX_86B2eFSA3jgGkNgVlL57Crd2RoIf8",
+  authDomain: "pixley-4632c.firebaseapp.com",
+  projectId: "pixley-4632c",
+  storageBucket: "pixley-4632c.firebasestorage.app",
+  messagingSenderId: "1006119196484",
+  appId: "1:1006119196484:web:d7c48070f62b9b42f0a911",
+  measurementId: "G-R56KM3Y9VD"
+};
